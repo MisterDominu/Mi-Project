6e05e318bffd453428223630227a600f77b131fb
MisterDominu <weelmeen@inbox.ru>
2022-09-08 13:23:07 +0400

    test
    test

---
+++ SoundBar/SoundBar.js
@@ -0,0 +1,44 @@
+
+let sound1 = document.querySelector("#a1");
+let sound2 = document.querySelector("#a2");
+let sound3 = document.querySelector("#a3");
+let sound4 = document.querySelector("#a4");
+let sound5 = document.querySelector("#a5");
+let sound6 = document.querySelector("#a6");
+let sound7 = document.querySelector("#a7");
+let sound8 = document.querySelector("#a8");
+
+let but1 = document.querySelector(".b1");
+let but2 = document.querySelector(".b2");
+let but3 = document.querySelector(".b3");
+let but4 = document.querySelector(".b4");
+let but5 = document.querySelector(".b5");
+let but6 = document.querySelector(".b6");
+let but7 = document.querySelector(".b7");
+let but8 = document.querySelector(".b8");
+
+but1.onclick = function (){
+    sound1.play();
+}
+
+but2.onclick = function (){
+    sound2.play();
+}
+but3.onclick = function (){
+    sound3.play();
+}
+but4.onclick = function (){
+    sound4.play();
+}
+but5.onclick = function (){
+    sound5.play();
+}
+but6.onclick = function (){
+    sound6.play();
+}
+but7.onclick = function (){
+    sound7.play();
+}
+but8.onclick = function (){
+    sound8.play();
+}
